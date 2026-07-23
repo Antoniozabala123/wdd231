d1c955ecd903c130bf7ead541034d46e96e5e8f1
Antoniozabala123 <lukinkeri051@gmail.com>
2026-07-23 05:04:20 -0400

    add

---
+++ chamber/scripts/home.js
@@ -0,0 +1,13 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+
+
+    hamButton.textContent = navigation.classList.contains('open') ? 'X' : '☰';
+});
+
+
