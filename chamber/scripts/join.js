14b4e3efd956c5bbf4b6a58f2e4eba91b6d35119
Antoniozabala123 <lukinkeri051@gmail.com>
2026-07-29 11:38:22 -0400

    add

---
+++ chamber/scripts/join.js
@@ -0,0 +1,33 @@
+const openButton1 = document.querySelector("#openbutton1");
+const openButton2 = document.querySelector("#openbutton2");
+const openButton3 = document.querySelector("#openbutton3");
+const openButton4 = document.querySelector("#openbutton4");
+const dialogbox = document.querySelector("#dialogBox");
+const dialogBoxText = document.querySelector("#dialogBox div");
+const closeButton = document.querySelector("#closeButton");
+
+// Evento para abrir (Corregido shadowModal por showModal)
+openButton1.addEventListener('click', () => {
+    dialogBoxText.innerHTML = "Access to community events, free resources, and more.";
+    dialogbox.showModal();
+});
+openButton2.addEventListener('click', () => {
+    dialogBoxText.innerHTML = "Discounted services, networking opportunities, and exclusive events.";
+    dialogbox.showModal();
+});
+openButton3.addEventListener('click', () => {
+    dialogBoxText.innerHTML = "Gold Membership Benefits";
+    dialogbox.showModal();
+});
+openButton4.addEventListener('click', () => {
+    dialogBoxText.innerHTML = "Gold Membership Benefits";
+    dialogbox.showModal();
+});
+
+
+// Evento para cerrar
+closeButton.addEventListener('click', () => {
+    dialogbox.close();
+});
+    
+
